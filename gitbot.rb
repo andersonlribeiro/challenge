a441bb0de476bf5ef4fe1ec8f4ea843fc4e82fd7
Giovanni Capuano <webmaster@giovannicapuano.net>
2015-12-02 16:20:33 +0100

    Add gitbot.rb
    Sample usage: `ruby gitbot.rb "Andrey Rapshtinskiy" "andrey-bh" be`
    
    Create a new private repository ("AndreyRapshtinskiy_be_001") inside this organization and invite the challenger (andrey-bh) there, setting him/her as collaborator with r/w permissions. Then, opens the repository inside your browser.

---
+++ gitbot.rb
@@ -0,0 +1,44 @@
+require 'github_api'
+require 'launchy'
+
+ORGANIZATION = 'honeypot-challenges'.freeze
+HOMEPAGE     = 'https://www.honeypot.io'.freeze
+VERSION      = '001'.freeze
+
+abort 'Usage: ruby gitbot.rb <full name> <username> <role>' if ARGV.length != 3
+
+roles = {
+  be: 'Backend',
+  fe: 'Frontend',
+  do: 'DevOps'
+}
+
+name, user = ARGV
+short_role = ARGV[2].to_sym
+role       = roles[short_role]
+abort 'Invalid role provided.' unless role
+
+repository_name = [
+  name.gsub(/\s+/, ''),
+  short_role,
+  VERSION
+].join('_')
+
+auth   = { username: ENV['GH_U'], password: ENV['GH_P'] }.values.join(':')
+github = Github.new(basic_auth: auth)
+
+github.repos.create(
+  org:  ORGANIZATION,
+  name: repository_name,
+  description: "#{role} Challenge v#{VERSION.to_i} #{name}",
+  private:  true,
+  has_wiki: false,
+  homepage: HOMEPAGE
+)
+
+github.repos.collaborators.add ORGANIZATION, repository_name, user
+
+# TODO: Push README
+
+url = ['https://github.com', ORGANIZATION, repository_name].join('/')
+Launchy.open(url)
