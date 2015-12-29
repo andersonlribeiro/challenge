5e2db13b44a80856ba10066ad7150462c62e1a5a
Giovanni Capuano <webmaster@giovannicapuano.net>
2015-12-29 13:07:17 +0100

    gitbot: allow input of version

--- scripts/gitbot.rb
+++ scripts/gitbot.rb
@@ -4,11 +4,11 @@ require 'launchy'
 
 ORGANIZATION = 'honeypot-challenges'.freeze
 HOMEPAGE     = 'https://www.honeypot.io'.freeze
-VERSION      = '001'.freeze
+VERSION      = 1.0
 COMMITTER    = { name: 'Honeypot Admin', email: 'admin@honeypot.io' }.freeze
 
-abort 'Usage: ruby gitbot.rb <full name> <username> <role>' if ARGV.length != 3
-abort 'Please set environment variables correctly'          unless ENV['GH_TOKEN']
+abort 'Usage: ruby gitbot.rb <full name> <username> <role> [version]' if ARGV.length < 3
+abort 'Please set environment variables correctly'                    unless ENV['GH_TOKEN']
 
 roles = {
   be: 'Backend',
@@ -19,14 +19,11 @@ roles = {
 
 name, user = ARGV
 short_role = ARGV[2].to_sym
+version    = ARGV.fetch(3, VERSION).to_i.to_s.rjust(3, '0')
 role       = roles[short_role]
 abort 'Invalid role provided.' unless role
 
-repository_name = [
-  user,
-  short_role,
-  VERSION
-].join('_')
+repository_name = [user, short_role, version].join('_')
 
 github = Github.new(oauth_token: ENV['GH_TOKEN'])
 
