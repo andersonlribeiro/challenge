759298c54b20dd54d5b00bbb6f2be19342a9a9d2
Giovanni Capuano <webmaster@giovannicapuano.net>
2015-12-29 12:53:02 +0100

    gitbot: improve code style

--- scripts/gitbot.rb
+++ scripts/gitbot.rb
@@ -5,9 +5,10 @@ require 'launchy'
 ORGANIZATION = 'honeypot-challenges'.freeze
 HOMEPAGE     = 'https://www.honeypot.io'.freeze
 VERSION      = '001'.freeze
+COMMITTER    = { name: 'Honeypot Admin', email: 'admin@honeypot.io' }.freeze
 
 abort 'Usage: ruby gitbot.rb <full name> <username> <role>' if ARGV.length != 3
-abort 'Please set environment variables correctly' unless ENV['GH_TOKEN']
+abort 'Please set environment variables correctly'          unless ENV['GH_TOKEN']
 
 roles = {
   be: 'Backend',
@@ -38,13 +39,17 @@ github.repos.create(
   homepage: HOMEPAGE
 )
 
-github.repos.collaborators.add ORGANIZATION, repository_name, user
+github.repos.collaborators.add(ORGANIZATION, repository_name, user)
 
-github.repos.contents.create ORGANIZATION, repository_name, 'README.md',
-  path: "README.md",
-  message: "Initial commit",
-  committer: {"name" => "Honeypot Admin", "email" => "admin@honeypot.io"},
-  content: File.read(File.expand_path(File.join(File.dirname(__FILE__), "..", "tasks", "#{short_role}_#{VERSION}.md")))
+task = File.read(File.expand_path(
+  File.join(File.dirname(__FILE__), '..', 'tasks', "#{short_role}_#{VERSION}.md")
+))
 
-url = ['https://github.com', ORGANIZATION, repository_name].join('/')
-Launchy.open(url)
+github.repos.contents.create(ORGANIZATION, repository_name, 'README.md',
+  path:     'README.md',
+  message:  'Initial commit',
+  committer: COMMITTER,
+  content:   task
+)
+
+Launchy.open("https://github.com/#{ORGANIZATION}/#{repository_name}.git")
